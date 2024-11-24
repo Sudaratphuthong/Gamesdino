95050c103fe1df2cd575b9ed8ab549d1c9b0205b
Sudaratphuthong <sudaratphuthong42@gmail.com>
2024-11-24 18:32:21 +0700

    Update App.js

--- dinogame/dino-game/src/App.js
+++ dinogame/dino-game/src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 import { Routes, Route } from 'react-router-dom';
 import Dino from "./components/Dino/Dino";
-import LoginRegister from "./components/login/login"
 import About from "./components/about/about"
 function App() {
   return (
     <>
        <Routes>
-          <Route path="/" element={<LoginRegister />} />
+          <Route path="/" element={<Dino />} />
           
-          <Route path="/dino" element={<Dino />} />
+        
           <Route path="/about" element={<About />} />
        </Routes>
     </>
