216b9afb842f0db20c5d890f3ffde2a56e22b9da
Sudaratphuthong <sudaratphuthong42@gmail.com>
2024-11-24 19:28:36 +0700

    Update login.js

--- dinogame/dino-game/src/components/login/login.js
+++ dinogame/dino-game/src/components/login/login.js
@@ -14,35 +14,39 @@ const LoginRegister = () => {
     const handleLogin = async (e) => {
         e.preventDefault(); // Prevent page refresh
 
-        try {
-            const response = await fetch(`${config.apiUrl}/api/register`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email, password }),
-            });
+        // try {
+        //     const response = await fetch(`${config.apiUrl}/api/register`, {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //         },
+        //         body: JSON.stringify({ email, password }),
+        //     });
 
-            if (!response.ok) {
-                throw new Error('Login failed');
-            }
+        //     if (!response.ok) {
+        //         throw new Error('Login failed');
+        //     }
 
-            const data = await response.json();
+        //     const data = await response.json();
 
-            // Handle successful login (e.g., store token, redirect)
-            console.log('Login successful:', data);
-            if (data.message === "User registered successfully") {
-                localStorage.setItem('user', JSON.stringify({ id: data.user.id, name: data.user.name, email: data.user.email }));
-                window.location.href = "/dino"
-            }
+        //     // Handle successful login (e.g., store token, redirect)
+        //     console.log('Login successful:', data);
+        //     if (data.message === "User registered successfully") {
+        //         localStorage.setItem('user', JSON.stringify({ id: data.user.id, name: data.user.name, email: data.user.email }));
+        //         window.location.href = "/dino"
+        //     }
 
-        } catch (err) {
-            //   setError(err.message);
-        }
+        // } catch (err) {
+        //     //   setError(err.message);
+        // }
+
+        window.location.href = "/dino"
     };
     const fetchScores = async () => {
         try {
-            const response = await fetch('https://dinosaur-facts-api.shultzlab.com/dinosaurs/random');
+            //  const response = await fetch('https://dinosaur-facts-api.shultzlab.com/dinosaurs/random');
+            const response = await fetch('https://api.thecatapi.com/v1/images/search');
+
 
             if (!response.ok) {
                 throw new Error('Failed to fetch scores');
@@ -68,70 +72,61 @@ const LoginRegister = () => {
         return () => clearInterval(interval);
     }, []);
 
-    console.log(data)
+    // console.log(data[0].url)
     console.log('All environment variables:', config.apiUrl);
 
     return (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ alignItems:'center' , display:'flex', paddingTop:'17%' ,height: '600px', width: '500px'}}>
+                {/* <p className="tip">{data.Name}</p> */}
+                {/* <p className="second-text">{data.Description}</p> */}
+                <img src={data[0]?.url} alt="Dynamic content" style={{ height: '100%', width: '100%' }} />
+            </div>
+            <div className="materialContainer">
+                
+                <div className={`box`}>
 
-        <div className="materialContainer">
-            <div className={`box`}>
-                <div className="title">{"LOGIN"}</div>
 
-                <div className="input">
-                    {/* <label htmlFor={isLogin ? "name" : "regname"}>{isLogin ? "Username" : "Username"}</label> */}
 
-                    <input
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        type="text" placeholder="Username" name={"name"} id={"name"} />
-                    <span className="spin"></span>
-                </div>
 
-                <div className="input">
-                    {/* <label htmlFor={isLogin ? "pass" : "regpass"}>{isLogin ? "Password" : "Password"}</label> */}
-                    <input
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        type={"password"} placeholder="Password" name={"pass"} id={"pass"} />
-                    <span className="spin"></span>
-                </div>
 
-                {/* {!isLogin && ( */}
-                {/* <div className="input">
+                    {/* {!isLogin && ( */}
+                    {/* <div className="input">
                         <label htmlFor="reregpass">Repeat Password</label>
                         <input type="password" name="reregpass" id="reregpass" />
                         <span className="spin"></span>
                     </div> */}
-                {/* )} */}
-                <div className={`button login`}>
-                    <button onClick={handleLogin}>
-                        <span>{"GO"}</span>
-                        <i className="fa fa-check"></i>
-                    </button>
+                    {/* )} */}
+                    <div className={`button login`}>
+                        <button onClick={handleLogin}>
+                            <span>{"GO"}</span>
+                            <i className="fa fa-check"></i>
+                        </button>
+                    </div>
+
+                    {/* {isLogin && ( */}
+                    {/* <a href="/" className="pass-forgot">Forgot your password?</a> */}
+                    {/* )} */}
                 </div>
 
-                {/* {isLogin && ( */}
-                {/* <a href="/" className="pass-forgot">Forgot your password?</a> */}
-                {/* )} */}
-            </div>
-
-            {/* <div className="overbox" onClick={toggleForm}>
+                {/* <div className="overbox" onClick={toggleForm}>
                 <div className="material-button alt-2">
                     <span className="shape"></span>
                 </div>
             </div> */}
 
-            <div className="cards">
-                <div className="card red">
-                    <p className="tip">{data.Name}</p>
-                    <p className="second-text">{data.Description}</p>
-                </div>
-                <div className="card blue">
-                <a href="/about"  style={{color:'#ffffff'}} >About</a>   
-                </div>
-            </div>
 
+                <div className="cards">
+
+                    <div className="card blue">
+                        <a href="/about" style={{ color: '#ffffff' }} >About</a>
+                    </div>
+                </div>
+
+            </div>
         </div>
+
+
 
     );
 };
